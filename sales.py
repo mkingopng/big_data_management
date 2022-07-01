6a39278d5289970cccb4f8dc505692e81e9d9b96
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2022-07-01 17:57:30 +1000

    initial commit

---
+++ sales.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+sales = "2543,4,4.34;5463,7,8.31;7765,10,1.23;9833,9,34.12;5056,4,5.67;7657,10,4.23;3343,7,2.98;3778,9,9.27;1118,5," \
+        "8.23;3873,3,4.45;6588,2,5.67;5778,6,3.41;7765,11,2.23;9343,8,4.12;5057,5,4.67;7657,5,4.23;3356,7,4.98;3776," \
+        "8,8.27;1228,5,7.23;3873,2,4.50"
+
+# split on the ';'
+data = sales.split(";")
+
+# split on the ','
+new_data = []
+for item in data:
+    new_item = item.split(",")
+    new_data.append(new_item)
+
+# convert a list of lists to a dataframe:
+df = pd.DataFrame(new_data)
+df.rename(columns={0: 'transaction_number', 1: 'quantity', 2: 'price_per_unit'}, inplace=True)
+df['quantity'] = pd.to_numeric(df['quantity'])
+df['price_per_unit'] = pd.to_numeric(df['price_per_unit'])
+df['sale_value'] = df['quantity'] * df['price_per_unit']  # calculate the value of each sale
+
+# sum the transaction column
+total = df['sale_value'].sum()
+print(total)
---
+++ words.py
@@ -0,0 +1,21 @@
+import re
+
+
+def avrg_count(x):
+    total_chars = len(re.sub(r'[^a-zA-Z0-9]', '', x))
+    num_words = len(re.sub(r'[^a-zA-Z0-9 ]', '', x).split())
+    print("Characters:{0}\nWords:{1}\nAverage word length: {2}".format(total_chars,
+                                                                       num_words,
+                                                                       round(total_chars/float(num_words), 2)))
+
+
+sentence = "In the loveliest town of all, where the houses were white and high and the elms trees were green and " \
+           "higher than the houses, where the front yards were wide and pleasant and the back yards were bushy and " \
+           "worth finding out about, where the streets sloped down to the stream and the stream flowed quietly under " \
+           "the bridge, where the lawns ended in orchards and the orchards ended in fields and the fields ended in " \
+           "pastures and the pastures climbed the hill and disappeared over the top toward the wonderful wide sky, in " \
+           "this loveliest of all towns Stuart stopped to get a drink of sarsaparilla."
+
+if __name__ == '__main__':
+    avrg_count(sentence)
+
